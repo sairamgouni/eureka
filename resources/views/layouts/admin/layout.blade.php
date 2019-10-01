585d7ec3b476622daf36acce7b812fbd6fb759db
sairamgouni <gounisairam@gmail.com>
2019-10-01 17:11:24 +0800

    admin panel name

--- resources/views/layouts/admin/layout.blade.php
+++ resources/views/layouts/admin/layout.blade.php
@@ -124,12 +124,12 @@
 
 					</div>
 				</div>
-				<a href="#" class="author-name fn">
-					<div class="author-title">
-					 <svg class="olymp-dropdown-arrow-icon"><use xlink:href="{{asset('assets/svg-icons/sprites/icons.svg')}}#olymp-dropdown-arrow-icon"></use></svg>
-					</div>
-
-				</a>
+                <a href="#" class="author-name fn">
+                    <div class="author-title">
+                        {{ Auth::user()->name }} <svg class="olymp-dropdown-arrow-icon"><use xlink:href="{{asset('assets/svg-icons/sprites/icons.svg')}}#olymp-dropdown-arrow-icon"></use></svg>
+                    </div>
+                    <span class="author-subtitle">{{ Auth::user()->campaign->campaign }}</span>
+                </a>
 			</div>
 
 		</div>
